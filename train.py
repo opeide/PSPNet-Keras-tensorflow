a38224ca2478c447079d8faad3b9d9126ba4f56f
opeide <fredrikopeide@gmail.com>
2019-03-20 18:57:57 +0100

    load xy training samples, parsing the color encoded y into n class masks

--- train.py
+++ train.py
@@ -1,20 +1,57 @@
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-from os.path import splitext, join, isfile
-from os import environ
-from math import ceil
-import argparse
+from os.path import join, isfile
 import numpy as np
-from scipy import misc, ndimage
-from keras import backend as K
-from keras.models import model_from_json
-import tensorflow as tf
-import layers_builder as layers
-import utils
-import matplotlib.pyplot as plt
-import cv2
-from tensorflow.python.client import device_lib
-from keras.optimizers import SGD
+#from keras.models import model_from_json
+#from keras.optimizers import SGD
+import glob
+from PIL import Image
+
+
+
+def get_x_y_data(dir_path, img_size=(473,473), n_classes=3, n_samples=-1):
+    #red-0-sky, green-1-land, blue-2-sea
+    mask_encodings = {(255,0,0):0, (0,255,0):1, (0,0,255):2}
+
+
+    img_paths = sorted(glob.glob('{}/images/*.png'.format(dir_path)))
+    if n_samples > 0:
+        n_samples = min(len(img_paths), n_samples)
+    else:
+        n_samples = len(img_paths)
+
+    x = np.empty((n_samples,img_size[0], img_size[1],3))
+    for img_path in img_paths[:n_samples]:
+        img = Image.open(img_path)
+        img.show()
+
+
+    mask_paths = sorted(glob.glob('{}/masks/*.png'.format(dir_path)))
+
+    y = np.empty((n_samples, img_size[0], img_size[1], n_classes))
+    for mask_path in mask_paths[:n_samples]:
+        mask_img = Image.open(mask_path)
+        mask_img.show()
+        mask_np = np.array(mask_img)
+        for color in mask_encodings:
+            class_id = mask_encodings[color]
+            print('class_id: {}'.format(class_id))
+            class_mask = np.all(mask_np == color, axis=2)
+            print(class_mask.shape)
+            Image.fromarray(class_mask.astype(dtype=np.uint8)*255, 'L').show()
+
+
+if __name__ == '__main__':
+    #model = get_compiled_model('pspnet50_custom', 1e-2)
+    train_path = '/home/fredrik/sensorfusion/world_tracker/segmentation/training_data'
+    get_x_y_data(train_path, n_samples=1)
+    exit()
+
+    n_img = len(x)
+    x_train = x[:int(0.8*n_img)]
+    y_train = y[:int(0.8 * n_img)]
+    x_val = x[int(0.8*n_img):]
+    y_val = y[int(0.8 * n_img):]
 
 
 def get_compiled_model(model_name, lrn_rate):
@@ -33,9 +70,6 @@ def get_compiled_model(model_name, lrn_rate):
                   metrics=['accuracy'])
     return model
 
-def get_train_test_data():
-    pass
-
 def overfit_test():
     model = get_compiled_model('pspnet50_custom', 1e-2)
 
@@ -53,7 +87,3 @@ def overfit_test():
                     batch_size=8,
                     epochs=1000,
                     validation_data=(x_val,y_val))
-
-
-if __name__ == '__main__':
-    overfit_test()
